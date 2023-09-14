53a33fbe5c5b6d30ad61801af0520ad9a833ca16
Samilincoln <agboolaola150@gmail.com>
2023-09-14 15:41:53 +0100

    adding the token auth and djoser library for security

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('menu/', views.MenuItemView.as_view(), name='Hello'),
-    path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='home')
+    path('menu-items/', views.MenuItemView.as_view(), name='Hello'),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view(), name='home'),
+    path('api-token-auth/', obtain_auth_token),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics, viewsets
 from .models import MenuItem, Booking
 from .serializers import UserSerializer, MenuItemSerializer, BookingSerializer
 from django.contrib.auth.models import User
-
+from rest_framework.permissions import IsAuthenticated
 
 
 
@@ -21,17 +21,25 @@ def home(request):
 class UserView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
     
 class MenuItemView (generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    #def itemview(request):
+    #    if request.method =='POST':
+
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset= MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
     
